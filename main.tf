f3ab83b8bd1375a7ea62b7da802c089caa19b198
Aris Kourtesas <ariskourt@gmail.com>
2021-10-11 13:32:51 +0300

    various changes

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region = var.stage
+  region = var.region
 }
 
 locals {
--- network.tf
+++ network.tf
@@ -1,33 +1,78 @@
 resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
-  instance_tenancy = "default"
   enable_dns_hostnames = true
+  enable_dns_support = true
+  tags = local.tags
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+  tags = local.tags
+}
+
+resource "aws_eip" "nat_eip" {
+  vpc = true
+  depends_on = [ aws_internet_gateway.igw ]
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = element(aws_subnet.public_subnet.*.id, 0)
+  depends_on = [ aws_internet_gateway.igw ]
   tags = local.tags
 }
 
 resource "aws_subnet" "public_subnet" {
   cidr_block = "10.0.1.0/24"
-  availability_zone = data.aws_availability_zones.region_zones.names[0]
-  map_public_ip_on_launch = true
   vpc_id     = aws_vpc.vpc.id
-
+  availability_zone = data.aws_availability_zones.zones.names[0]
+  map_public_ip_on_launch = true
   tags = local.tags
 }
 
 resource "aws_subnet" "private_subnet" {
   cidr_block = "10.0.2.0/24"
-  availability_zone = data.aws_availability_zones.region_zones.names[0]
-  map_customer_owned_ip_on_launch = false
   vpc_id     = aws_vpc.vpc.id
+  availability_zone = data.aws_availability_zones.zones.names[0]
+  map_public_ip_on_launch = false
+  tags = local.tags
 }
 
-resource "aws"
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
+  tags = local.tags
+}
 
-data "aws_availability_zones" "region_zones" {
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.vpc.id
+  tags = local.tags
+}
 
+resource "aws_route" "public_igw" {
+  route_table_id = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw.id
+}
+
+resource "aws_route" "private_nat" {
+  route_table_id = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.nat.id
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  route_table_id = aws_route_table.public.id
+  subnet_id = aws_subnet.public_subnet.id
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  subnet_id = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private.id
+}
+
+data "aws_availability_zones" "zones" {
   filter {
     name   = "region-name"
     values = [ var.region ]
   }
-
 }
